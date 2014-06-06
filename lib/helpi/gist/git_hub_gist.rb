4550db03db3a54bea0154752364180cb6b64cf06
Theo Pack <tf.pack@gmail.com>
2014-06-06 17:08:04 +0200

    add github gist

--- lib/helpi/gist/git_hub_gist.rb
+++ lib/helpi/gist/git_hub_gist.rb
@@ -1,5 +1,25 @@
-class GitHubGist
-  def create
+require 'rest_client'
+require 'json'
 
+class GitHubGist
+  def initialize(access_token)
+    @access_token = access_token
+  end
+
+  def create
+    result = RestClient::Resource.new 'https://api.github.com/gists'
+    response = result.post create_gist(nil), :Authorization => "token #{@access_token}"
+    JSON.parse(response)["html_url"]
+  end
+
+  private
+  def create_gist(filename)
+    '{
+        "description": "the description for this gist",
+        "public": true,
+        "files": {
+          "file1.txt": { "content": "String file contents!!!!!!" }
+        }
+     }'
   end
 end
