75d4e36cc9659e99d2a41037f4e52a6d7dc2f671
Theo Pack <tf.pack@gmail.com>
2014-06-09 14:30:56 +0200

    add tests

--- spec/helpi/gist_spec.rb
+++ spec/helpi/gist_spec.rb
@@ -1,7 +1,16 @@
 require 'helpi/gist'
+require 'helpi/github/git_hub_gist'
+require 'helpi/github/git_hub_authorization'
 require_relative '../spec_helper'
 
 describe Gist do
+
+  it "can upload a file to github gist" do
+    allow_any_instance_of(GitHubAuthorization).to receive(:access_token) {'access_token'}
+    allow_any_instance_of(GitHubGist).to receive(:create)
+    gist = Gist.new
+    expect(gist.run('user', 'pw', 'file')).to eq("Gist")
+  end
 
   it "has a help text" do
     gist = Gist.new
