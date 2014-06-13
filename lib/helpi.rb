e0af0a5d8453f7a8d1bd0a8ba55df9bfb81a37c8
Theo Pack <tf.pack@gmail.com>
2014-06-13 15:07:10 +0200

    add required files

--- lib/helpi.rb
+++ lib/helpi.rb
@@ -1,6 +1,9 @@
+require File.dirname(__FILE__) + '/helpi/formatter'
+require File.dirname(__FILE__) + '/helpi/gist'
+require File.dirname(__FILE__) + '/helpi/translate'
 
 module Helpi
   def self.version
-    "0.0.2"
+    "0.0.3"
   end
 end
